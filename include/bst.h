1f06c6d73cea2369f7cd645e6e7d4b7e7a7caf99
absltn000 <78900100+absltn000@users.noreply.github.com>
2021-06-06 17:55:42 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,23 +2,23 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 #include <string>
+#include <minmax.h>
 
 template<typename T>
 class BST {
- private:
+private:
   struct dot {
     T t;
-    unsigned char height;
     dot* left;
     dot* right;
     int count;
-    dot(T m) : height(1), left(nullptr), right(nullptr), count(1) {
-     t = m;
+    dot(T m) :left(nullptr), right(nullptr), count(1) {
+      t = m;
     }
   };
   dot* root;
 
- public:
+public:
   BST() : root(nullptr) {};
 
   void insert(T m) {
@@ -30,90 +30,60 @@ class BST {
     while (y != nullptr) {
       if (x == y->t) {
         return y->count;
-      } else if (x > y->t) {
+      }
+      else if (x > y->t) {
         y = y->right;
-      } else {
+      }
+      else {
         y = y->left;
       }
     }
     return 0;
   }
 
-  int search2(std::string x) {
+  void search2(std::string x) {
+    if (root == nullptr) {
+      root = new dot(x);
+      return;
+    }
     dot* y = root;
     while (y != nullptr) {
       if (x == y->t) {
         y->count++;
-        return y->count;
+        return;
       } else if (x > y->t) {
-        y = y->right;
+        if (y->right == nullptr) {
+          y->right = new dot(x);
+          return;
+        } else {
+          y = y->right;
+        } 
       } else {
-        y = y->left;
+        if (y->left == nullptr) {
+          y->left = new dot(x);
+          return;
+        } else {
+          y = y->left;
+        }
       }
     }
-    root = insert(root, x);
-    return 0;
+    return;
   }
 
   int depth() {
-    return height1(root);
+    return hight(root) - 1;
   }
 
- private:
-  unsigned char height1(dot* p) {
-    return p ? p->height:0;
-  }
-
-  int bfactor(dot* p) {
-    return height1(p->right) - height1(p->left);
-  }
-
-  void fixheight(dot* p) {
-    unsigned char hl = height1(p->left);
-    unsigned char hr = height1(p->right);
-    p->height = (hl > hr ? hl : hr) + 1;
-  }
-
-  dot* rotateright(dot* p) {
-    dot* q = p->left;
-    p->left = q->right;
-    q->right = p;
-    fixheight(p);
-    fixheight(q);
-    return q;
-  }
-
-  dot* rotateleft(dot* q) {
-    dot* p = q->right;
-    q->right = p->left;
-    p->left = q;
-    fixheight(q);
-    fixheight(p);
-    return p;
-  }
-
-  dot* balance(dot* p) {
-    fixheight(p);
-    if (bfactor(p) == 2) {
-      if (bfactor(p->right) < 0)
-        p->right = rotateright(p->right);
-      return rotateleft(p);
+  int hight(dot* p) {
+    if ((p->left != nullptr) && (p->right != nullptr)) {
+      return max(hight(p->left), hight(p->right)) + 1;
+    } else if (p->left != nullptr) {
+      return hight(p->left) + 1;
+    } else if (p->right != nullptr) {
+      return hight(p->right) + 1;
+    } else {
+      return 1;
     }
-    if (bfactor(p) == -2) {
-      if (bfactor(p->left) > 0)
-        p->left = rotateleft(p->left);
-      return rotateright(p);
-    }
-    return p;
-  }
-
-  dot* insert(dot* p, T m) {
-    if (!p) return new dot(m);
-    if (m < p->t)
-      p->left = insert(p->left, m);
-    else
-      p->right = insert(p->right, m);
-    return balance(p);
   }
 };
 
