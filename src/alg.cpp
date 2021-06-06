0081b03da762651f82c1ceaf3cc93d222a7ab1aa
absltn000 <78900100+absltn000@users.noreply.github.com>
2021-06-06 13:47:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,20 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> tree;
+  std::ifstream file(filename);
+  std::string str1 = "";
+  while (!file.eof()) {
+    char x = file.get();
+    if ((x > 64) && (x < 91)) {
+      str1 += (x + 32);
+    } else if ((x > 96) && (x < 123)) {
+      str1 += x;
+    } else if (str1 != "") {
+      tree.search2(str1);
+      str1 = "";
+    }
+  }
+  file.close();
+  return tree;
 }
